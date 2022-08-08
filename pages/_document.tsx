27e4bcec40fc6234547cb93852a76d6e2397396e
Fahd <flaslam@gmail.com>
2022-08-08 18:09:12 +0100

    add google analytics scripts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(
@@ -14,10 +15,19 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;700;800;900&display=swap"
-            rel="stylesheet"
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
+
+          <Script strategy="lazyOnload" id="gtag">
+            {` window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+              }); `}
+          </Script>
         </Head>
         <body>
           <Main />
