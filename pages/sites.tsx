927b0d171ff93f86138806a86d6ececa5d7cafd2
Fahd <flaslam@gmail.com>
2022-11-26 20:08:47 +0000

    add back button to front-end page, fix typo

--- pages/sites.tsx
+++ pages/sites.tsx
@@ -3,6 +3,7 @@ import { sites } from "../types/projects";
 import Layout from "../components/layouts/layout";
 import { motion } from "framer-motion";
 import ProjectsLayout from "../components/projects-layout";
+import Back from "../components/back";
 
 const Projects: NextPage = () => {
   return (
@@ -12,6 +13,9 @@ const Projects: NextPage = () => {
         animate={{ opacity: 1, translateY: 0 }}
       >
         <div className="container mx-auto mb-12 px-6 pb-8 sm:px-12">
+          <div className="mb-4 lg:mb-0">
+            <Back>Back to projects</Back>
+          </div>
           <ProjectsLayout projects={sites} />
         </div>
       </motion.div>
