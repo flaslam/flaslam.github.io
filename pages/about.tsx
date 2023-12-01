2cab2a0bd2613fce57fd7c52f0c83c4506bc264f
Fahd <flaslam@gmail.com>
2023-12-01 23:38:36 +0000

    update about

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,9 +5,9 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 
 const About: NextPageWithLayout = () => {
   const technologies = [
-    "C# (Unity)",
-    "JavaScript ES6",
+    "C#",
     "TypeScript",
+    "JavaScript",
     "React",
     "Next.js",
     "Node.js",
@@ -15,7 +15,6 @@ const About: NextPageWithLayout = () => {
     "Python",
     "SQL",
     "MongoDB",
-    "HTML & CSS",
   ];
 
   return (
@@ -23,9 +22,8 @@ const About: NextPageWithLayout = () => {
       <div className="container mx-auto px-container">
         <div className="mx-6">
           <div className="mx-auto flex flex-col gap-6 rounded-md bg-brand-light px-8 py-8 dark:bg-dark-panel">
-            <p>Full stack web and indie game developer based in London, UK</p>
+            <p>Full stack developer + indie game dev based in London, UK</p>
             <div className="flex flex-col gap-2">
-              <div>Technologies I have been working with:</div>
               <ul className="grid grid-cols-2 sm:grid-cols-3 sm:gap-2 md:grid-cols-4">
                 {technologies.map((item, index) => (
                   <li key={index} className="ml-5 list-disc marker:text-link">
